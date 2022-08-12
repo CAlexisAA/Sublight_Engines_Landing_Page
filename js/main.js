2dbe15e19a334075e6df5a39368d177cab88ef0c
CAlexisAA <c.alexisalm.a@gmail.com>
2022-08-12 14:47:13 -0400

    2nd week

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassme('toggle-button')[0]
-const navbarLinks = document.getElementById('nav-ul') [0]
+const navbarLinks = document.getElementsIdName('navbar-ul') [0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.toggleButton('active')
