9da5ca812550c1fe0b71c65c79942703aa55f1e4
CAlexisAA <c.alexisalm.a@gmail.com>
2022-08-12 14:41:16 -0400

    2nd week

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassme('toggle-button')[0]
+const navbarLinks = document.getElementById('nav-ul') [0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.toggleButton('active')
+})
